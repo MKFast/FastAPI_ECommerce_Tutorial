10e26dc55c4af26b0b6eb2a9f9c726495857c521
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2021-07-19 20:39:15 +0430

    adding alembic tutorial file

--- FastAPI_Alembic_Tutorial/main.py
+++ FastAPI_Alembic_Tutorial/main.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 import crud
 import models
+import schemas
 from database import engine, SessionLocal
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,8 +18,10 @@ def get_db():
         db.close()
 
 @app.post("/create/")
-def create_product(title:str,description:str,price:int,db: Session = Depends(get_db)):
-    return crud.create_product(title=title,description=description,price=price,db=db)
+def create_product(product:schemas.Product,db: Session = Depends(get_db)):
+    return crud.create_product(title=product.title
+                               ,description=product.description,
+                               price=product.price,db=db)
 
 @app.get("/list/")
 def product_list(db: Session = Depends(get_db)):
