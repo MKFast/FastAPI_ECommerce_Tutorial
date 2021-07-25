d9310d57c6c0b55a57bcc340710e620474496ac0
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2021-07-25 21:36:54 +0430

    update productList version

---
+++ ECommerce_ProductList_Model_Tutorial/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///sql_app.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args = {"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, bind=engine)
+
+Base = declarative_base()
+
--- ECommerce_ProductList_Tutorial/dependencies.py
+++
@@ -1,9 +0,0 @@
-from database import sessionLocal
-
-def get_db():
-    db = sessionLocal
-    try:
-        yield db
-    finally:
-        db.close()
-
